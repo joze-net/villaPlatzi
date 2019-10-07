c98b322372814bcbbe2898eba5ab6daa85958fc9
joze-net <50345325+joze-net@users.noreply.github.com>
2019-10-07 17:39:44 -0500

    se creo una funcion importante con la ecuacion para generar numeros aleatorios

---
+++ aleatorio.js
@@ -0,0 +1,11 @@
+
+var min=10;
+var max=20;
+
+for(var i=0;i<10;i++){
+
+	var resultado=Math.floor(Math.random()*(max-min+1)+min);//ecuacion para calcular el numero aleatrio entre un minimo y un maximo
+    document.write(resultado + ", ");
+}
+
+
